5169ded1fcb1f1a196a84f189a782052ed2b618b
Adam-Koza <adam.koza@mail.utoronto.ca>
2018-11-27 16:02:01 -0500

    Change BootSrap version.

--- index.js
+++ index.js
@@ -5,9 +5,22 @@ function search() {
         url: "https://images-api.nasa.gov/search?q=apollo%2011&description=" + encoded + "&media_type=image",
         success: function(response){
             console.log(response);
-            for (i=0;i<3;i++){
+            var addString = '';
+            for (i=0;i<12;i++){
                 console.log(response.collection.items[i]);
+                addString +=
+                `<div class="card">
+                <img class="card-img-top" src="` + response.collection.items[i].links[0].href + `" alt="Card image cap">
+                <div class="card-body">
+                  <h5 class="card-title">`+ response.collection.items[i].data[0].title + `</h5>
+                  <p class="card-text">` + response.collection.items[i].data[0].description + `</p>
+                </div>
+                </div>`;
+                if (i == 3 || i === 7) {
+                    addString += `</div><div class="card-deck">`;
+                }
             }
+            $('#results').html(addString);
         }
     });
 
