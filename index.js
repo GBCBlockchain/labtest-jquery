b196a4a19b5feecdd0865f41a3d707ef88bd53ff
Adam-Koza <adam.koza@mail.utoronto.ca>
2018-11-27 15:10:12 -0500

    Basic search function.

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 
 function search() {
+    var encoded = encodeURIComponent($('#term').val());
+    $.get({
+        url: "https://images-api.nasa.gov/search?q=apollo%2011&description=" + encoded + "&media_type=image",
+        success: function(response){
+            console.log(response);
+            for (i=0;i<3;i++){
+                console.log(response.collection.items[i]);
+            }
+        }
+    });
+
+
 
 }
