3254a6201dfba391db5efd3a0b4fd5a7067860a2
yanesh <yaneshcode@gmail.com>
2018-11-26 20:38:59 -0500

    added image parsing

--- script.js
+++ script.js
@@ -1,22 +1,28 @@
- $(document).ready(function(e) {   
+$(document).ready(function(e) {   
 
-     
-   $(':submit').click(function (event) {
-      event.preventDefault();  // Do not run the default action
-      
-      let searchQuery = $(':text[name="searchq"]').val();
-      $.get("https://images-api.nasa.gov/search?q=" + searchQuery, function(data, status) {
-  
-          
-          if(status == "success") {
-                      console.log(data);
-          } else {
-            alert("Error retreiving images from NASA. Status", status);
-            console.log(status, data);
-          }
-          
-      });
-   }
- });
- 
- 
+
+    $('#submit').click(function (event) {
+        event.preventDefault();  // Do not run the default action
+
+        let searchQuery = $(':text[name="searchq"]').val();
+        $.get("https://images-api.nasa.gov/search?q=" + searchQuery, function(data, status) {
+
+
+            if(status == "success") {
+                console.log(data);
+                let counter = 0;
+                while(counter < 12) {
+                    $('p:first').after('<img src="' + data.collection.items[counter].links[0].href + '"></img>');
+                    counter++;
+                }
+            } else {
+                alert("Error retreiving images from NASA. Status", status);
+                console.log(status, data);
+            }
+
+        });
+    });
+
+});
+
+
