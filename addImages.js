312faad71ab86a283e7128899f25e37850f5f4af
MarkCBall <moball@hotmail.com>
2018-11-26 21:48:35 -0500

    finalized project - description pops up on mouseover

--- addImages.js
+++ addImages.js
@@ -34,31 +34,23 @@
             //if we have not reached the end of the images
             if (imagesJSON.collection.items[itemID]){
                 let image = imagesJSON.collection.items[itemID].href;
-                //use IVF - immediately invoked function to pass variable to callback inside loop
+                //use IVF - immediately invoked function - to pass variable to callback inside loop
                 let descrip = imagesJSON.collection.items[itemID].data[0].description;
                 (function(descr){
                     //get the image urls from the json collection of images
                     $.getJSON(image, function (imageJSON){
-                        console.log(itemID)
                         //append a url in imageJSON if one exists, otherwise try again
-                        console.log(descr)
                         if (!appendImage(imageJSON, descr)){
                             showNextX(imagesJSON,1)
-                            
                         }
-                        //else 
-                        //$("p").append(imagesJSON.collection.items[itemID].data[0].description+"<br>")
                     });
                 })(descrip)//end IVF
-                
-
             }else {
                 //let the user know there are no more images to display
                 console.log("ran out of items");
                 $("#showMore").text("Nothing more to show").css({ 'color': 'red', 'font-size': '150%' });;
             }     
         }
-        //$("#showMore").append((imagesJSON.collection.items.length - itemID) + " more pics to show")
     }
 
     //called to append a specific version of an image from the JSON including many versions of that file
@@ -78,7 +70,7 @@
     //function to append a single file to the html file
     function appendFileLink(url, desc){
         if (urlIsImg(url) )
-            $("p").append("<img width='132' height='124' src='"+url+"'><br>"+desc)
+            $("p").append("<img width='132' height='124' src='"+url+"' title='"+desc+"'>")
         else 
             console.log("unsupported file type: " + url)
     }
